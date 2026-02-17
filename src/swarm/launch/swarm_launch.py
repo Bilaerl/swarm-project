bdbce2dccf6a555ac6d0e684fa6fde669f893e85
Bilal Babale <bilalbabale@gmail.com>
2026-02-17 13:42:42 +0100

    Modify launch file to launch Gazebo Sim using ros_gz_sim package
    This is the standard way of launching Gazebo Sim from ros2, not the
    cmd hack I was initially using

--- src/swarm/launch/swarm_launch.py
+++ src/swarm/launch/swarm_launch.py
@@ -1,24 +1,39 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 
 # gets paths to the gazebo worlds and models in the share directory
-path_prefix = get_package_share_directory('swarm')
-gz_sim_world_file = os.path.join(path_prefix, 'worlds', 'swarm-world.sdf')
-gz_sim_models_folder = os.path.join(path_prefix, 'models')
+path_prefix = get_package_share_directory("swarm")
+gz_sim_world_file = os.path.join(path_prefix, "worlds", "swarm-world.sdf")
+gz_sim_models_folder = os.path.join(path_prefix, "models")
+
+# gets the path to the gz_sim launch file in the ros_gz_sim package
+# this is needed to launch gazebo with the swarm world and models
+gz_sim_package_dir = get_package_share_directory("ros_gz_sim")
+gz_sim_package_launch_file = os.path.join(gz_sim_package_dir, "launch", "gz_sim.launch.py")
 
 
 def generate_launch_description():
+    # needed so gazebo can find the swarm world and models when launched
+    gz_sim_env_variables = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=gz_sim_models_folder
+    )
 
-    gz_sim_node = ExecuteProcess(
-        cmd=[f"export GZ_SIM_RESOURCE_PATH={gz_sim_models_folder}; $(which gz) sim", gz_sim_world_file],
-        name="gz_sim",
-        shell=True
+    # using the ros_gz_sim package to launch gazebo
+    gz_sim_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(gz_sim_package_launch_file),
+        launch_arguments={
+            "gz_args": gz_sim_world_file,
+            "on_exit_shutdown": "True",
+        }.items()
     )
 
     return LaunchDescription([
-        gz_sim_node,
+        gz_sim_env_variables,
+        gz_sim_launch,
     ])

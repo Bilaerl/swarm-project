7d79fbb2999ca6a8225df1c885f1b0543abf31e7
Bilal Babale <bilalbabale@gmail.com>
2026-02-25 10:53:28 +0100

    Remove rviz config from install files

--- src/swarm/launch/swarm_launch.py
+++ src/swarm/launch/swarm_launch.py
@@ -84,7 +84,6 @@ def generate_launch_description():
         package="rviz2",
         executable="rviz2",
         name="rviz",
-        arguments=["-d", rviz_config_file_path],
         output="screen"
     )
 
